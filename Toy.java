b6328dcf7329b6bc625b00723010313a745c41e3
jpst26 <16576312+paolotrrj26@users.noreply.github.com>
2026-07-14 12:02:17 +0800

    Day 13 - basics

---
+++ Toy.java
@@ -0,0 +1,20 @@
+public class Toy {
+    //Initializations
+    
+    String name, brand;
+    double price;
+    int quantity;
+
+    void setPrice(double price) {
+        this.price = price;
+    }
+    public static void main(String[] args) {
+        Toy toy1 = new Toy();
+        toy1.name = "Rage Pink";
+        toy1.brand = "Lab Vuvu";
+        toy1.price = 4500;
+        toy1.quantity = 12;
+        toy1.setPrice((toy1.price*0.5));
+        System.out.println(toy1.price);
+    }
+}
