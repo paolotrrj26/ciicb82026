14b41814782fb38f5540e24d83dae4da18f28a5a
Juan Paolo Tarroja <16576312+paolotrrj26@users.noreply.github.com>
2026-07-15 13:30:30 +0800

    Added Task1.java - for task 1 submission

---
+++ chapter1/Tasks/Task1.java
@@ -0,0 +1,10 @@
+package ciicb82026.chapter1.Tasks;
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    }
+}
