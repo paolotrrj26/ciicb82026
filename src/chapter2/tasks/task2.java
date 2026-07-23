26617777687f3ae663b74dafe6ca8ac19ca60461
Juan Paolo Tarroja <16576312+paolotrrj26@users.noreply.github.com>
2026-07-23 12:09:34 +0800

    Task2.java - Task 2 submission

---
+++ src/chapter2/tasks/task2.java
@@ -0,0 +1,18 @@
+package chapter2.tasks;
+
+public class task2 {
+    
+    public static void main(String[] args) {
+        
+        byte byteVal = 0;
+        short shortVal = 1;
+        int intVal = 3110;
+        float floatVal = 2.0f;
+        boolean booleanVal = true;
+        char charVal = 'H';
+
+        //H3110 w0r1d 2.0 true
+        String concatOut = charVal + "" + intVal + " " + "w" + byteVal + "r" + shortVal + "d" + " " + floatVal + " " + booleanVal;
+        System.out.println(concatOut);
+    }
+}
